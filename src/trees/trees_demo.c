a1524f29da32252d1ab06739398b8e99dbb8cf57
jagritiuppal158 <jagritiuppal158@gmail.com>
2026-07-06 18:06:19 +0530

    refactor(trees): replace magic numbers with TreeChoice enum in trees_demo

--- src/trees/trees_demo.c
+++ src/trees/trees_demo.c
@@ -3,15 +3,27 @@
 #include "trees.h"
 #include <stdio.h>
 
+typedef enum
+{
+    CHOICE_BST = 1,
+    CHOICE_AVL,
+    CHOICE_TBT,
+    CHOICE_TRIE,
+    CHOICE_BTREE,
+    CHOICE_BPLUS,
+    CHOICE_SEGMENT
+} TreeChoice;
+
 void trees_demo(void)
 {
-    int tree_status, tree_choice;
+    int tree_status;
+    TreeChoice tree_choice;
 
     while (1)
     {
         display_header("Trees");
 
-        tree_status = safe_input_int(&tree_choice,
+        tree_status = safe_input_int((int*)&tree_choice,
                                      "\nenter 1 for Binary Search Tree demo"
                                      "\nenter 2 for AVL Tree demo"
                                      "\nenter 3 for Threaded Binary Tree demo"
@@ -33,37 +45,37 @@ void trees_demo(void)
 
         switch (tree_choice)
         {
-            case 1:
+            case CHOICE_BST:
                 display_header("Binary Search Tree");
                 binary_search_tree_demo();
                 break;
 
-            case 2:
+            case CHOICE_AVL:
                 display_header("AVL Tree");
                 avl_demo();
                 break;
 
-            case 3:
+            case CHOICE_TBT:
                 display_header("Threaded Binary Tree");
                 TBT_demo();
                 break;
 
-            case 4:
+            case CHOICE_TRIE:
                 display_header("Trie");
                 trie_demo();
                 break;
 
-            case 5:
+            case CHOICE_BTREE:
                 display_header("B-Tree");
                 btree_demo();
                 break;
 
-            case 6:
+            case CHOICE_BPLUS:
                 display_header("B+ Tree");
                 bplus_tree_demo();
                 break;
 
-            case 7:
+            case CHOICE_SEGMENT:
                 display_header("Segment Tree");
                 segment_tree_demo();
                 break;
