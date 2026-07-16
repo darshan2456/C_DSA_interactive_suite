4f5740893d24306d74e7add1a27cffea4efc96a2
dev-akshatshukla <akshat21shukla@gmail.com>
2026-07-16 23:47:38 +0530

    Fix radix sort negative value stack buffer overflow (#871)

--- src/advanced_sorting_algorithms/radix_sort.c
+++ src/advanced_sorting_algorithms/radix_sort.c
@@ -59,6 +59,14 @@ void radix_sort(int arr[], int n)
         return;
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            return;
+        }
+    }
+
     int max = get_max(arr, n);
 
     for (int exp = 1; max / exp > 0; exp *= 10)
