d583f3ad5cc9c737cb680543deada9553dfbe2ca
jagritiuppal158 <jagritiuppal158@gmail.com>
2026-07-23 15:28:39 +0530

    feat(bits): implement rightmost bit ops and bit reversal

--- src/bit_manipulation/advanced_ops.c
+++ src/bit_manipulation/advanced_ops.c
@@ -27,7 +27,7 @@ int is_power_of_two(int n)
 /**
  * Swaps two integers in place using the XOR operator without a temporary variable.
  */
-void xor_swap(int *a, int *b)
+void xor_swap(int* a, int* b)
 {
     if (a != b)
     {
@@ -35,4 +35,39 @@ void xor_swap(int *a, int *b)
         *b = *a ^ *b;
         *a = *a ^ *b;
     }
+}
+
+/**
+ * Returns the rightmost set bit of an integer.
+ * This is effectively isolating the lowest set bit.
+ */
+int get_rightmost_set_bit(int n)
+{
+    return n & -n;
+}
+
+/**
+ * Turns off (clears) the rightmost set bit of an integer.
+ */
+int turn_off_rightmost_set_bit(int n)
+{
+    return n & (n - 1);
+}
+
+/**
+ * Reverses the bits of a 32-bit unsigned integer.
+ */
+unsigned int reverse_bits(unsigned int n)
+{
+    unsigned int res = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        res <<= 1;
+        if (n & 1)
+        {
+            res |= 1;
+        }
+        n >>= 1;
+    }
+    return res;
 }
